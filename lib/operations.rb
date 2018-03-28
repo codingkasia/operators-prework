2d210269b676537549420a7d67ca697f83229972
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-28 21:04:25 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,32 +3,20 @@ def unsafe?(speed)
     return true
     elsif speed >40 && speed <=60
     return false
+  end
 end
 
 
 
 def not_safe?(speed)
+  if speed > 60 || speed < 40
+    return true
+    elsif speed >=40 $$ speed <=60
+    return false
+  end
+end
   
-   it 'returns true if speed is greater than 60' do
-      answer = not_safe?(79)
-      expect(answer).to eq(true)
-    end
-
-    it 'returns true if speed is less than 40' do
-      answer = not_safe?(35)
-      expect(answer).to eq(true)
-    end
-
-    it 'returns false if the speed is between 40 and 60' do
-      answer = not_safe?(50)
-      expect(answer).to eq(false)
-    end
-
-    it 'uses the ternary operator' do
-      methods = file_contents.split("not_safe?")
-      match = methods.last
-      expect(match).to include("?")
-    end
+  
 	
 end
 	
